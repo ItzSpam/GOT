2d8b2f8acce06e8e9342668a835cf58192b790b8
ItzSpam <44649434+ItzSpam@users.noreply.github.com>
2019-02-28 21:25:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "13";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
 
-client.on("ready", () => {
-let channel =     client.channels.get("523983138615984149")
-setInterval(function() {
-channel.send(`LOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLO
-LOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLO
-LOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLO
-LOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLOLOLOLOLOLOOOOOOOOLOLOLOLOLO`);
-}, 1)
-})
+const developers = ["550758894163460097"]
+const adminprefix = "#";
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅ تم تغير اللعبة الى  ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();   
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅ تم تغير المشاهدة الى  ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅  تم تغير الاستماع الى ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/BlueGamer295YT");
+      message.channel.send(`**✅ تم تغير الستريم الى ${argresult}**`)
   }
- });
-
-
-
-
-
-
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+  if (message.content.startsWith(adminprefix + 'setprefix')) {
+  client.user.setPrefix(argresult).then
+      message.channel.send(`Changing Prefix ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `)
+}
+});
  
 client.login(process.env.BOT_TOKEN);
